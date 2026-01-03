b3a17795b49304ca384b1f2ada8125d9a247d80f
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-04 00:21:45 +0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,11 +1,12 @@
 import random
+# creating a list of choices holding three possible user choices
 choices = ["rock","paper","scissors"]
 choice = random.choice(choices)
 user_choice = input("Enter your choice(rock,paper,scissors) : ").strip().lower()
-
+# handling an invalid choice entered by user which is not present in choices list 
 if user_choice not in choices:
     print("Invalid Choice")
-
+# handling the case when user enters "paper"
 elif user_choice == "paper":
     if choice == "scissors":
         print("You Lose")
@@ -13,7 +14,7 @@ elif user_choice == "paper":
         print("It's a Tie")
     else:
         print("You Win")
-
+# handling the case when user enters "rock"
 elif user_choice == "rock":
     if choice == "paper":
         print("You Lose")
@@ -21,7 +22,7 @@ elif user_choice == "rock":
         print("It's a Tie")
     else:
         print("You Win")
-
+# handling the case when user enters "scissors"
 elif user_choice == "scissors":
     if choice == "rock":
         print("You Lose")
