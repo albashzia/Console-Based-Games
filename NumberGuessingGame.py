4c56bca490db8f2f94c7d6e86254b2609df2e376
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-05 18:39:46 +0500

    Update NumberGuessingGame.py by commenting code

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,15 +1,22 @@
 import random
+# welcome statements
 print("Number Guessing Game")
 print("Welcome to the Number Guessing Game")
-attempts = 0
-secret_number = random.randint(1,10)
-while True:
+
+attempts = 0 # counter for number of attempts
+
+secret_number = random.randint(1,10) # generating a random number between 1 and 10
+
+while True: # running until the correct number is guessed
     num = int(input("Enter a number between 1 and 10 : "))
-    attempts = attempts + 1
+    attempts = attempts + 1 # incrementing number of attempts on each loop
+    
     if num > secret_number:
         print("High")
+        
     elif num<secret_number:
         print("Low")
+        
     elif num == secret_number:
         print("You guessed it right in",attempts,"attempts")
-        break
+        break # breaking once the correct number is guessed
