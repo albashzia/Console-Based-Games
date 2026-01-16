5a262b55fa2aea479e337e22dcc2cc6de32af06e
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-16 22:37:29 +0500

    Using a list to hold all the questions to be asked from the user

---
+++ python_basics_quiz.py
@@ -0,0 +1,12 @@
+questions = [
+    "What is the correct file extension for a Python file?",
+    "Which keyword is used to define a function in Python?",
+    "What will print(type(5)) output?",
+    "Which of the following is a valid variable name in Python?",
+    "What does the len() function do?",
+    "Which symbol is used for comments in Python?",
+    "What will print(10 // 3) output?",
+    "Which data type is used to store True or False values?",
+    "What is the output of print(\"Hello\" + \"World\")?",
+    "Which function is used to take input from the user in Python?"
+]
