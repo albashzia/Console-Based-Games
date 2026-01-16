ea0dc8182d687ac7cef24a848db516dca79bf1a5
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-16 22:38:54 +0500

    Adding options list holding the options for all the questions to be asked to  python_basics_quiz.py

--- python_basics_quiz.py
+++ python_basics_quiz.py
@@ -10,3 +10,16 @@ questions = [
     "What is the output of print(\"Hello\" + \"World\")?",
     "Which function is used to take input from the user in Python?"
 ]
+
+options =[
+    "a) .py b) .pt c) .python d) .pyt",
+    "a) func b) define c) def d) function",
+    "a) <class 'float'> b) <class 'int'> c) <class 'str'> d) <class 'bool'>",
+    "a) 2name b) name_2 c) name-2 d) @name",
+    "a) Adds numbers b) Counts characters/items c) Converts type d) Prints output",
+    "a) // b) /* */ c) # d) --",
+    "a) 3.33 b) 3 c) 4 d) 10",
+    "a) int b) float c) bool d) str",
+    "a) Hello World b) HelloWorld c) Hello+World d) Error",
+    "a) scan() b) read() c) input() d) get()"
+]
