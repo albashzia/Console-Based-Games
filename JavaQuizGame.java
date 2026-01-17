a0124703727d2c85139ec5f3f2f64720175d8df4
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-17 23:04:40 +0500

    Add quiz questions to JavaQuizGame

--- JavaQuizGame.java
+++ JavaQuizGame.java
@@ -1,5 +1,18 @@
 public class JavaQuizGame {
     public static void main(String[] args) {
+
+        String questions[] = {
+            "Which keyword is used to define a class in Java?",
+            "What is the file extension of a Java source file?",
+            "Which method is the entry point of a Java program?",
+            "Which data type is used to store whole numbers in Java?",
+            "Which keyword is used to create an object in Java?",
+            "What does System.out.println() do?",
+            "Which symbol is used to end a statement in Java?",
+            "Which of the following is NOT a Java primitive data type?",
+            "What is the default value of an int variable in Java (instance variable)?",
+            "Which keyword is used to inherit a class in Java?"
+        };
         
     }
 }
